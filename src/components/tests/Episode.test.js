caa50bc3fb17ba8aa178e1b5a199c76be5969321
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-16 14:28:45 -0500

    All episode component tests passing

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,27 +8,48 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "This is a test episode summary.",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+    id: 2,
+    name: '',
+    image: '',
+    season: 1,
+    number: 2,
+    summary: '',
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+  render(<Episode episode={testEpisode} />)
 })
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", ()=>{
+  render(<Episode episode={testEpisode} />)
+  const summary = screen.queryByText(/This is a test episode summary/i)
+  expect(summary).toBeInTheDocument()
+  expect(summary).toBeTruthy()
+  expect(summary).toHaveTextContent('This is a test episode summary.')
 })
 
 test("renders default image when image is not defined", ()=>{
-    
+  render(<Episode episode={testEpisodeWithoutImage}/>)
+  const alt = screen.getAllByAltText('./stranger_things.png')
+  expect(alt).toBeTruthy()
 })
 
-//Tasks
-//1. Complete a test that shows the Episode component renders. Pass in the provided example episode data as a test prop.
-//2. Modify the test data to display a specific summary statement. Complete a test that shows that the summary value passed in to the Episode component displays as expected. Use no more then 3 different expect statements to test the the existance of the summary value.
-//3. The episode component displays a default value ('./stranger_things.png') when a image url is not provided. Create a new piece of test data with the image property set to null. Test that the alt tag of the image displayed is set to './stranger_things.png'.
+/*
+  Tasks
+  1. Complete a test that shows the Episode component renders. Pass in the 
+     provided example episode data as a test prop.
+  2. Modify the test data to display a specific summary statement. Complete 
+     a test that shows that the summary value passed in to the Episode component 
+     displays as expected. Use no more then 3 different expect statements to test 
+     the the existence of the summary value.
+  3. The episode component displays a default value ('./stranger_things.png') 
+     when a image url is not provided. Create a new piece of test data with the 
+     image property set to null. Test that the alt tag of the image displayed is 
+     set to './stranger_things.png'.
+*/
